7a7e19e5f4f382f7f591331976f621f2003170a2
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-09-18 16:19:03 +0200

    remove utc info for datetime-local

--- collective/z3cform/html5widgets/widget_datetimelocal.py
+++ collective/z3cform/html5widgets/widget_datetimelocal.py
@@ -66,5 +66,13 @@ class DateTimeLocalConverter(DateTimeConverter):
             return ''
         return value.strftime('%Y-%m-%dT%H:%M')
 
+
+    def toFieldValue(self, value):
+        value = DateTimeConverter.toFieldValue(self, value)
+        if value is not self.field.missing_value:
+            #make it utc unaware
+            value.replace(tzinfo=None)
+        return value
+
     def raise_error(self):
         raise DateTimeLocalValidationError
